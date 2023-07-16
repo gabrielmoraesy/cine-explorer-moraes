9814c04a7d7c029937a9d0617230ce69d549f3df
gabrielmoraesy <ygabrielmoraes@gmail.com>
2023-07-16 19:25:55 -0300

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
